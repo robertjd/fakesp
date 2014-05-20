b3a5c76f80f742502344293ef95e1696a09484bd
Robert <robert@stormpath.com>
2014-05-20 12:14:32 -0700

    use the jwtResponse to show account info

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-
-
+var url = require('url');
 var stormpath = require('stormpath');
 
 
@@ -27,18 +26,38 @@ stormpath.loadApiKey(API_KEY_FILE, function apiKeyFileLoaded(err, apiKey) {
 
 http.createServer(function (req, res) {
   console.log(req.headers.host,req.method,req.headers['content-type'] || '',req.url);
+  var params = url.parse(req.url,true).query;
   if(req.url==='/login'){
     res.writeHead(302, {
       'Cache-Control': 'no-store',
       'Pragma': 'no-cache',
       'Location': application.createSsoUrl({
-        redirect_uri: DOMAIN + ':' + PORT,
-        path: '/'
+        cb_uri: 'http://' + DOMAIN + ':' + PORT,
+        path: ':9000/',
+        state: 'whaaatuppp'
       })
     });
     res.end();
+  }else if(params.jwtResponse){
+    application.parseSsoResponse(req.url,function(err,account){
+      if(err){
+        res.writeHead(500, {
+          'Cache-Control': 'no-store',
+          'content-type': 'text/html',
+          'Pragma': 'no-cache'
+        });
+        res.end(fs.readFileSync('error.html').toString().replace('ERROR',err));
+      }else{
+        res.writeHead(200, {
+          'Cache-Control': 'no-store',
+          'content-type': 'text/html',
+          'Pragma': 'no-cache'
+        });
+        res.end(fs.readFileSync('account.html').toString().replace('ACCOUNT',JSON.stringify(account,null,2)));
+      }
+    });
   }else{
-    res.writeHead(302, {
+    res.writeHead(200, {
       'Cache-Control': 'no-store',
       'content-type': 'text/html',
       'Pragma': 'no-cache'
