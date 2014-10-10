beb61f7bab50abec0263c07771d7dbce009510e7
Robert <robert@stormpath.com>
2014-10-09 17:43:14 -0700

    an example that uses sessions on the service provider side

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-var http = require('http');
 var fs = require('fs');
-var url = require('url');
 var stormpath = require('stormpath');
-var open = require('open');
+var openBrowser = require('open');
 
 var client, application;
 var IS_PRODUCTION = process.env.NODE_ENV==='production';
@@ -16,83 +14,114 @@ var SSO_SITE_PATH = process.env.SSO_SITE_PATH || '';
 var CB_URI = process.env.CB_URI || ('http://' + DOMAIN + ':' + PORT);
 
 
+var express = require('express');
+var app = express();
+var sessions = require('client-sessions');
+app.use(sessions({
+  cookieName: 'sp', // cookie name dictates the key name added to the request object
+  secret: STORMPATH_API_KEY_SECRET, // should be a large unguessable string
+  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+  activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+}));
+
+app.get('/', function(req, res){
+  if(req.query.jwtResponse){
+    application.handleIdSiteCallback(req.url,function(err,idSiteResult){
+      if(err){
+        res.writeHead(500, {
+          'Cache-Control': 'no-store',
+          'content-type': 'text/html',
+          'Pragma': 'no-cache'
+        });
+        res.end(fs.readFileSync('error.html').toString().replace('ERROR',err));
+      }else{
+        req.sp.accountHref = idSiteResult.account.href;
+        res.redirect('/');
+      }
+    });
+  }else if(req.sp && req.sp.accountHref){
+    client.getAccount(req.sp.accountHref,function(err,account){
+      if(err){
+        res.writeHead(500, {
+          'Cache-Control': 'no-store',
+          'content-type': 'text/html',
+          'Pragma': 'no-cache'
+        });
+        res.end(fs.readFileSync('error.html').toString().replace('ERROR',err));
+      }else{
+        res.writeHead(200, {
+          'Cache-Control': 'no-store',
+          'content-type': 'text/html',
+          'Pragma': 'no-cache'
+        });
+        res.end(fs.readFileSync('account.html').toString().replace('ACCOUNT',JSON.stringify(account,null,2)));
+      }
+    });
+  }else{
+    res.writeHead(200, {
+      'Cache-Control': 'no-store',
+      'content-type': 'text/html',
+      'Pragma': 'no-cache'
+    });
+    res.end(fs.readFileSync('index.html'));
+  }
+});
+
+app.get('/login', function(req, res){
+  res.writeHead(302, {
+    'Cache-Control': 'no-store',
+    'Pragma': 'no-cache',
+    'Location': application.createIdSiteUrl({
+      callbackUri: CB_URI,
+      path: SSO_SITE_PATH
+    })
+  });
+  res.end();
+});
+
+app.get('/logout', function(req, res){
+
+  if(req.query.jwtResponse){
+    res.writeHead(302, {
+      'Cache-Control': 'no-store',
+      'Pragma': 'no-cache',
+      'Location': '/'
+    });
+    res.end();
+  }else{
+    req.sp.destroy();
+    res.writeHead(302, {
+      'Cache-Control': 'no-store',
+      'Pragma': 'no-cache',
+      'Location': application.createIdSiteUrl({
+        callbackUri: CB_URI + '/logout',
+        logout: true
+      })
+    });
+    res.end();
+  }
+});
+
+
+
 function startServer(){
   console.log('attempt to start server on port ' + PORT);
-  http.createServer(function (req, res) {
-    console.log(req.headers.host,req.method,req.headers['content-type'] || '',req.url);
-    var params = url.parse(req.url,true).query;
-    if(req.url==='/login'){
-      res.writeHead(302, {
-        'Cache-Control': 'no-store',
-        'Pragma': 'no-cache',
-        'Location': application.createIdSiteUrl({
-          callbackUri: CB_URI,
-          path: SSO_SITE_PATH
-        })
-      });
-      res.end();
-    }else if(req.url.match(/\/logout/)){
-      if(params.jwtResponse){
-        res.writeHead(302, {
-          'Cache-Control': 'no-store',
-          'Pragma': 'no-cache',
-          'Location': '/'
-        });
-        res.end();
-      }else{
-        res.writeHead(302, {
-          'Cache-Control': 'no-store',
-          'Pragma': 'no-cache',
-          'Location': application.createIdSiteUrl({
-            callbackUri: CB_URI + '/logout',
-            logout: true
-          })
-        });
-        res.end();
-      }
-
-    }else if(params.jwtResponse){
-      application.handleIdSiteCallback(req.url,function(err,account){
-        if(err){
-          res.writeHead(500, {
-            'Cache-Control': 'no-store',
-            'content-type': 'text/html',
-            'Pragma': 'no-cache'
-          });
-          res.end(fs.readFileSync('error.html').toString().replace('ERROR',err));
-        }else{
-          res.writeHead(200, {
-            'Cache-Control': 'no-store',
-            'content-type': 'text/html',
-            'Pragma': 'no-cache'
-          });
-          res.end(fs.readFileSync('account.html').toString().replace('ACCOUNT',JSON.stringify(account,null,2)));
-        }
-      });
-    }else{
-      res.writeHead(200, {
-        'Cache-Control': 'no-store',
-        'content-type': 'text/html',
-        'Pragma': 'no-cache'
-      });
-      res.end(fs.readFileSync('index.html'));
-    }
-  }).listen(PORT,function(){
+  app.listen(PORT,function(){
+    console.log('Server running on port '+PORT);
     if(!IS_PRODUCTION){
-      open('http://'+DOMAIN+':'+PORT);
+      openBrowser('http://'+DOMAIN+':'+PORT);
     }
   });
 
-  console.log('Server running on port '+PORT);
 }
 
-function getApplication(){
+function getApplication(then){
   client.getApplication(STORMPATH_APP_HREF,function(err,a){
     if (err){
       throw err;
     }
     application = a;
-    startServer();
+    then();
   });
 }
 
@@ -111,5 +140,5 @@ if(API_KEY_FILE){
       STORMPATH_API_KEY_SECRET
     )
   });
-  getApplication();
+  getApplication(startServer);
 }
